810f45a8d613e3c7ae08c7b13d3c4c85f44cd2d2
CJHJW <ck1305100872@163.com>
2025-02-09 00:08:46 -0500

    2/9 CK Image_tests

--- p2-image-processing/starter-files/Image_tests.cpp
+++ p2-image-processing/starter-files/Image_tests.cpp
@@ -66,7 +66,7 @@ TEST(test_width_height) {
 
 // Sets various pixels in two 2x2 Images and checks
 // that Image_fill correctly fill these Images.
-TEST(test_fill_pixel) {
+TEST(test_fill_pixel_1) {
   Image img;
   Image img1;
   const Pixel red = {255, 0, 0};
@@ -106,6 +106,91 @@ TEST(test_fill_pixel) {
   }
 
   ASSERT_TRUE(Image_equal(&img, &img1));
+
+}
+
+// Sets various pixels in one 3x3 Image and checks
+// that Image_fill correctly fill the Image.
+TEST(test_fill_pixel_2) {
+  Image img2;
+  Image_init(&img2, 3, 3);
+
+  const Pixel red = {255, 0, 0};
+  const Pixel green = {0, 255, 0};
+  const Pixel blue = {0, 0, 255};
+  const Pixel white = {255, 255, 255};
+  const Pixel new_color_2 = {1, 2, 3};
+
+  Image_set_pixel(&img2, 0, 0, white);
+  Image_set_pixel(&img2, 0, 1, blue);
+  Image_set_pixel(&img2, 0, 2, blue);
+  Image_set_pixel(&img2, 1, 0, green);
+  Image_set_pixel(&img2, 1, 1, red);
+  Image_set_pixel(&img2, 1, 2, blue);
+  Image_set_pixel(&img2, 2, 0, green);
+  Image_set_pixel(&img2, 2, 1, red);
+  Image_set_pixel(&img2, 2, 2, blue);
+
+  Image_fill(&img2, new_color_2);
+
+  // Check each pixel
+  for (int i = 0; i < Image_width(&img2); i++) {
+    for (int j = 0; j < Image_height(&img2); j++) {
+      ASSERT_TRUE(Pixel_equal(Image_get_pixel(&img2, i, j), new_color_2));
+    }
+  }
+
+}
+
+// Sets various pixels in one 1x1 Image and checks
+// that Image_fill correctly fill the Image.
+TEST(test_fill_pixel_3) {
+  Image img;
+  Image_init(&img, 1, 1);
+
+  const Pixel red = {255, 0, 0};
+  const Pixel new_color = {1, 2, 3};
+
+  Image_set_pixel(&img, 0, 0, red);
+
+  Image_fill(&img, new_color);
+
+  // Check each pixel
+  for (int i = 0; i < Image_width(&img); i++) {
+    for (int j = 0; j < Image_height(&img); j++) {
+      ASSERT_TRUE(Pixel_equal(Image_get_pixel(&img, i, j), new_color));
+    }
+  }
+
+}
+
+// Sets various pixels in one 2x3 Image and checks
+// that Image_fill correctly fill the Image.
+TEST(test_fill_pixel_4) {
+  Image img;
+  Image_init(&img, 3, 2);
+
+  const Pixel red = {255, 0, 0};
+  const Pixel green = {0, 255, 0};
+  const Pixel blue = {0, 0, 255};
+  const Pixel new_color = {1, 2, 3};
+
+  Image_set_pixel(&img, 0, 0, red);
+  Image_set_pixel(&img, 0, 1, green);
+  Image_set_pixel(&img, 0, 2, blue);
+  Image_set_pixel(&img, 1, 0, blue);
+  Image_set_pixel(&img, 1, 1, red);
+  Image_set_pixel(&img, 1, 2, green);
+
+  Image_fill(&img, new_color);
+
+  // Check each pixel
+  for (int i = 0; i < Image_width(&img); i++) {
+    for (int j = 0; j < Image_height(&img); j++) {
+      ASSERT_TRUE(Pixel_equal(Image_get_pixel(&img, j, i), new_color));
+    }
+  }
+
 }
 
 // Read dog.ppm file to initialize the Image.
--- p2-image-processing/starter-files/Matrix.cpp
+++ p2-image-processing/starter-files/Matrix.cpp
@@ -52,8 +52,8 @@ int Matrix_height(const Matrix* mat) {
 // EFFECTS:  Returns a pointer to the element in the Matrix
 //           at the given row and column.
 int* Matrix_at(Matrix* mat, int row, int column) {
-  assert(0 <= row && row < mat->height);
-  assert(0 <= column && column < mat->width);
+  assert(0 <= row && row < Matrix_height(mat));
+  assert(0 <= column && column < Matrix_width(mat));
   int *ptr = &mat->data[column+row*mat->width];
   return ptr;
 }
@@ -65,8 +65,8 @@ int* Matrix_at(Matrix* mat, int row, int column) {
 // EFFECTS:  Returns a pointer-to-const to the element in
 //           the Matrix at the given row and column.
 const int* Matrix_at(const Matrix* mat, int row, int column) {
-  assert(0 <= row && row < mat->height);
-  assert(0 <= column && column < mat->width);
+  assert(0 <= row && row < Matrix_height(mat));
+  assert(0 <= column && column < Matrix_width(mat));
   const int *ptr = &mat->data[column+row*mat->width];
   return ptr;
 }
