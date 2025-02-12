6e2a876de9dc414348f0646400a824b01a3a6d7f
Shamon Gajraj <shamon.gajraj@gmail.com>
2025-02-11 22:36:22 -0500

    Finished resize.cpp implementation

--- p2-image-processing/starter-files/resize.cpp
+++ p2-image-processing/starter-files/resize.cpp
@@ -3,20 +3,68 @@
 #include <fstream>
 
 int main(int argc, char *argv[]) {
-  Image img;
+  
 //   Error Checking https://eecs280staff.github.io/image-processing/#error-checking
-//   bool check_1;
-//   bool check_2;
-//   bool check_3;
-//   bool check_4;
-//   if (check_1 || check_2 || check_3 || check_4) {
+  
 
-//   }
+  bool correct_arg_count = (argc == 4 || argc == 5);
+  // Ensures correct number of arguments are inputted
+  if (!correct_arg_count)
+  {
+    std::cout << "Usage: resize.exe IN_FILENAME OUT_FILENAME WIDTH [HEIGHT]\n"
+    << "WIDTH and HEIGHT must be less than or equal to original" << std::endl;
+
+    return 1;
+  }
+  
+  // Checks if input file could be opened
   std::ifstream inFile(argv[1]);
+  if (!inFile) {
+    std::cout << "Error opening file: " << argv[1] << std::endl;
+    return 1;
+  }
+
+  // Checks if output file could be opened
   std::ofstream outFile(argv[2]);
+  if (!outFile) {
+    std::cout << "Error opening file: " << argv[2] << std::endl;
+    return 1;
+  }
+
+  Image img;
   Image_init(&img, inFile);
+
   int new_width = std::stoi(argv[3]);
-  int new_height = std::stoi(argv[4]);
+  int new_height;
+
+  // If no desired height was inputted, new_height is set to
+  // the original height of image. Else, inputted height is stored
+  if (argc == 4)
+  {
+    new_height = Image_height(&img);
+  }
+  else
+  {
+    new_height = std::stoi(argv[4]);
+  }
+  
+
+  bool correct_desired_width = (0 < new_width &&
+   new_width <= Image_width(&img));
+  bool correct_desired_height = (0 < new_height &&
+   new_height <= Image_height(&img));
+
+  // Checks if new_height and new_width are in the correct range
+  if (!correct_desired_width|| !correct_desired_height) {
+    std::cout << "Usage: resize.exe IN_FILENAME OUT_FILENAME WIDTH [HEIGHT]\n"
+    << "WIDTH and HEIGHT must be less than or equal to original" << std::endl;
+
+    return 1;
+  }
+
+
   seam_carve(&img, new_width, new_height);
   Image_print(&img, outFile);
+
+  return 0;
 }
