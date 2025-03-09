26803da192b27785d8f115fa510b24e4038a6906
CJHJW <ck1305100872@163.com>
2025-03-09 01:47:55 -0500

    3/9 Setup Table

--- p3-euchre/starter-files/euchre.cpp
+++ p3-euchre/starter-files/euchre.cpp
@@ -1,9 +1,41 @@
 #include "Player.hpp"
+#include "Pack.hpp"
 #include <string>
 #include <iostream>
 #include <fstream>
 
 using namespace std;
+
+class Game {
+    public:
+        // Set up the game: set pack, create players
+        Game(string pack_in, string shuffle, string points,
+             string player1, string type1,
+             string player2, string type2,
+             string player3, string type3,
+             string player4, string type4);
+             
+        // Play the game
+        void play();
+
+    private:
+        std::vector<Player*> players;
+        Pack pack;
+        bool shuffle;
+        int win_point;
+        int group1;
+        int group2;
+        int hand;
+        int dealer;
+        // ...
+        
+        void shuffle();
+        void deal(/* ... */);
+        void make_trump(/* ... */);
+        void play_hand(/* ... */);
+        // ...
+
+};
 
 int main(int argc, char *argv[]) {
 
@@ -27,7 +59,8 @@ int main(int argc, char *argv[]) {
 
     // Checks if the shuffle argument passed is valid
     string shuffle_argument = argv[2];
-    bool correct_shuffle_arg = (shuffle_argument == "shuffle" || shuffle_argument == "noshuffle");
+    bool correct_shuffle_arg = (shuffle_argument == "shuffle" || 
+                                shuffle_argument == "noshuffle");
 
     // Checks if the types of player 0, 1, 2, 3 are valid (Simple or Human)
     // [5 & 7 & 9 & 11] 
@@ -65,6 +98,123 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-
-
+    
 }
+
+class Game {
+    public:
+        Game(string pack_in, string shuffle, string points,
+             string player1, string type1,
+             string player2, string type2,
+             string player3, string type3,
+             string player4, string type4) {
+
+                // Step1: Set the pack
+                ifstream pack_input(pack_in);
+                pack = Pack(pack_input);
+                
+                // Step2: Whether shuffle
+                if (shuffle == "shuffle") {
+                    shuffle = true;
+                }
+                else {
+                    shuffle = false;
+                }
+
+                // Step3: Points to win:
+                win_point = stoi(points);
+
+                // Step4: Create players
+                players[0] = Player_factory(player1, type1);
+                players[1] = Player_factory(player2, type2);
+                players[2] = Player_factory(player3, type3);
+                players[3] = Player_factory(player4, type4);
+
+                // Step5: Initialize each group's point to 0
+                group1 = 0;
+                group2 = 0;
+
+                //Step6: Initialize hand(# of rounds) and dealer index
+                hand = 0;
+                dealer = 0;
+
+            }
+        void play() {
+
+            // Continue the game until one group reaches the win_point
+            while ((group1 < win_point) && (group2 < win_point)) {
+
+                // For each hand:
+
+                // Step0: start the hand
+                hand ++;
+
+                // Step1: Shuffle if require
+                if (shuffle) {
+                    pack.shuffle();
+                }
+
+                // Step2: Deal Cards for each player
+
+                // 2.1 Locate the Dealer index
+                dealer = (hand - 1) % 4;
+
+                // 2.2 Deal by 3-2-3-2
+                for (int i = 1; i <= 4; ++i) {
+                    if (i % 2 == 1) {
+                        for (int j = 0; j < 3; ++j) {
+                            players[(dealer+i)%4]->add_card(pack.deal_one());
+                        }
+                    }
+                    else {
+                        for (int j = 0; j < 2; ++j) {
+                            players[(dealer+i)%4]->add_card(pack.deal_one());
+                        }
+                    }
+                }
+
+                // 2.3 Deal by 2-3-2-3
+                for (int i = 1; i <= 4; ++i) {
+                    if (i % 2 == 1) {
+                        for (int j = 0; j < 2; ++j) {
+                            players[(dealer+i)%4]->add_card(pack.deal_one());
+                        }
+                    }
+                    else {
+                        for (int j = 0; j < 3; ++j) {
+                            players[(dealer+i)%4]->add_card(pack.deal_one());
+                        }
+                    }
+                }
+
+                // 2.4 Get the upcard‘s suit
+                Suit upcard = pack.deal_one().get_suit();
+
+
+            }
+
+
+            // Final Step: Delete players, end game
+            for (size_t i = 0; i < players.size(); ++i) {
+                delete players[i];
+            }
+        }
+
+    private:
+        std::vector<Player*> players;
+        Pack pack;
+        bool shuffle;
+        int win_point;
+        int group1;
+        int group2;
+        int hand;
+        int dealer;
+        // ...
+        
+        void shuffle();
+        void deal(/* ... */);
+        void make_trump(/* ... */);
+        void play_hand(/* ... */);
+        // ...
+
+};
